df34aab86008196b2c3d4fcd321571eb2c29af26
github-action-benchmark <github@users.noreply.github.com>
2021-03-18 02:53:29 +0000

    add Benchmark (cargo) benchmark result for 11802a9a1d35c2573abdb79447e13b40ba27ff83

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1616036009935,
+  "repoUrl": "https://github.com/lukechu10/maple",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "37006668+lukechu10@users.noreply.github.com",
+            "name": "Luke Chu",
+            "username": "lukechu10"
+          },
+          "committer": {
+            "email": "37006668+lukechu10@users.noreply.github.com",
+            "name": "Luke Chu",
+            "username": "lukechu10"
+          },
+          "distinct": true,
+          "id": "11802a9a1d35c2573abdb79447e13b40ba27ff83",
+          "message": "Setup continuous benchmark\n\nPush continuous benchmark results to gh_pages\n\nFix cargo bench command",
+          "timestamp": "2021-03-17T19:49:56-07:00",
+          "tree_id": "4f61562eff826f8fe365e54b622b63c998cd79b7",
+          "url": "https://github.com/lukechu10/maple/commit/11802a9a1d35c2573abdb79447e13b40ba27ff83"
+        },
+        "date": 1616036009473,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "reactivity_signals",
+            "value": 49388,
+            "range": "± 3692",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "reactivity_effects",
+            "value": 408867,
+            "range": "± 24368",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
